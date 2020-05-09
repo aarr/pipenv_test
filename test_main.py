93ac837888b4def4477ac3be260a5ee79ee52571
aarr <arar0529@gmail.com>
2020-05-09 14:08:10 +0900

    changed

--- test_main.py
+++ test_main.py
@@ -5,9 +5,6 @@ import pytest
 def teardown1():
     print('teardown1 complete')
 
-def teardown2():
-    print('teardown2 complete')
-
 
 # fixture定義を引数で取るテスト関数で有効になる。
 # module単位の定義をしても引数で渡されている関数で初めて実行される
@@ -15,50 +12,55 @@ def teardown2():
 # default   -> テストメソッド毎に実行
 # "module"  -> モジュール毎に１度実行
 # "session" -> テスト全体で１度実行
-@pytest.fixture(scope="module")
-def setup(request):
-    class Hoge:
-        def __init__(self, value):
-                super().__init__()
-                self.hoge = value
-
-        def get_hoge(self):
-            return self.hoge
-
-        def set_hoge(self, value):
-            self.hoge = value
-    
+@pytest.fixture(scope='module')
+def con(request, connection):
+    # 前処理    
     print('')
-    print('setup complete')
-    hoge = Hoge('aaaaa')
-
+    print('before yeild process complete')
+    # 後処理２登録
     # finalizerは複数登録できる
     request.addfinalizer(teardown1)
-    request.addfinalizer(teardown2)
-    yield hoge
+    request.addfinalizer(lambda con =connection : print('teardown2 complete. connection_name:{0}'.format(con.name)))
 
+    # テスと実行
+    yield connection 
+
+    # 後処理１
     # yieldに達する前にエラーになった場合、これ以降の処理も行われない。
     # 確実に終了処理を行うためには、コンテキストにfinalizerを追加すること
     print('')
     print('after yeild process complete')
-    hoge.set_hoge('bbbbb')
+    # finalizerよりも先に実行される。
+    # オブジェクトの値を変更することが可能
+    # テストケース毎にこれを実行すると、test_connection_nameがエラーとなる。
+    # test_connection_name以前のテストケースの後処理によって名前が書き換わるため。
+    connection.name = 'ChangedConnection'
 
 
-def test_zero_division():
-    """エラーケーステスト
-    """
-    with pytest.raises(ZeroDivisionError):
-        1 / 0
 
-def test_recursion_depth(setup):
-    with pytest.raises(RuntimeError) as excinfo:
-        def f():
+# テストケース毎にtempディレクトリの初期化を行うなどが可能
+# テストメソッド毎の設定も可能
+# テスト全体で利用するのであれば、iniファイルに記載も可能
+@pytest.mark.usefixtures('cleandir')
+class TestFixuture():
+
+    def test_zero_division(self, con):
+        """エラーケーステスト
+        """
+        with pytest.raises(ZeroDivisionError):
+            1 / 0
+
+    def test_recursion_depth(self, con):
+        with pytest.raises(RuntimeError) as excinfo:
+            def f():
+                f()
             f()
-        f()
-    assert 'maximum recursion' in str(excinfo.value)
+        assert 'maximum recursion' in str(excinfo.value)
 
-def test_setup(setup):
-    assert 'aaaaa' == setup.get_hoge()
 
-def test_1():
+    def test_connection_name(self, con):
+        assert 'TestConnection1' == con.name
+
+@pytest.mark.usefixtures('cleandir')
+def test_1(con):
     assert 1 == 1
